ed304121a46124ec4665ced4db266db7cdfcb268
Python3-Pyro <73122844+Python3-Pyro@users.noreply.github.com>
2024-04-07 19:58:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,36 +6,38 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask(__name__)
-
-client = MongoClient(os.getenv("MONGODB_URI"))
-app.db = client.microblog
-
-client = OpenAI()
-
-@app.route("/", methods =["GET", "POST"])
-def chat_bot(): 
-  # print([e for e in app.db.entries.find({})])                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-  if request.method == "POST":
-    entry_content = request.form.get("user_content")
-    
-    
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "system", "content": "You are a helpful bot who answers questions that are being asked"},
-      {"role": "user", "content": entry_content}
-    ]
-    )
-    
-    bot_content = completion.choices[0].message.content
-    
-    # entries.append((entry_content, bot_content))
-    app.db.entries.insert_one({"user_content": entry_content, "response": bot_content})    
-
-  entries_with_response = [(entry["user_content"], entry["response"]) for entry in app.db.entries.find({})]
+def app_create():
+  app = Flask(__name__)
   
-  if entries_with_response:
-    return render_template("index.html", entries=entries_with_response)
-  else:
-    return render_template("index.html")
+  client = MongoClient(os.getenv("MONGODB_URI"))
+  app.db = client.microblog
+  
+  client = OpenAI()
+  
+  @app.route("/", methods =["GET", "POST"])
+  def chat_bot(): 
+    # print([e for e in app.db.entries.find({})])                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+    if request.method == "POST":
+      entry_content = request.form.get("user_content")
+      
+      
+      completion = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=[
+        {"role": "system", "content": "You are a helpful bot who answers questions that are being asked"},
+        {"role": "user", "content": entry_content}
+      ]
+      )
+      
+      bot_content = completion.choices[0].message.content
+      
+      # entries.append((entry_content, bot_content))
+      app.db.entries.insert_one({"user_content": entry_content, "response": bot_content})    
+  
+    entries_with_response = [(entry["user_content"], entry["response"]) for entry in app.db.entries.find({})]
+    
+    if entries_with_response:
+      return render_template("index.html", entries=entries_with_response)
+    else:
+      return render_template("index.html")
+    return app
